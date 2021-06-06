e44383589e95267665aafd82b8b3e91d6f4a180e
Christian <c_alonso_10@hotmail.com>
2021-06-06 16:59:43 -0600

    Sin div

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*Preguntar:
+-Si se puede programar los eventos desde el js
+-Cross origin request are only support protocol scemes: el dominio en el que esta el sitio no es el mismo dominio que el datos/texto
+servidor web WampServer,www root de loar proyectos
+*/
+/*funcion load*/
+function cargar(){
+	$("#texto").load("../datos/texto.txt");
+	alert("todo tuanis");
+	
+}
